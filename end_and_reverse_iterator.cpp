d2f8cea45555552b6cb1237e9845e2f4c343aa90
Stranger4uu <yashsaini4824@gmail.com>
2025-11-20 19:59:39 +0530

    added imp comments

--- end_and_reverse_iterator.cpp
+++ end_and_reverse_iterator.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main(){
 
     vector<int> yash  = {10, 20, 30, 40, 50};
-    vector<int>::iterator it1 = yash.begin();
+    vector<int>::iterator it1 = yash.begin(); // imp
     it1++;
-    vector<int>::iterator it2 = yash.end();
+    vector<int>::iterator it2 = yash.end(); // imp
     it2--;
-    vector<int>::reverse_iterator rit = yash.rend();
+    vector<int>::reverse_iterator rit = yash.rend(); //not important or never used
     rit--;
-    vector<int>::reverse_iterator rit2 = yash.rbegin();
+    vector<int>::reverse_iterator rit2 = yash.rbegin(); //not important or never used
     rit2++;
     cout << *(it1) << " ";
     cout<<endl;
